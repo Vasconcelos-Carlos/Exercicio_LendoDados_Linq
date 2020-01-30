5f55c14e963ba65236bfacce5ff563f48148f2c6
Carlos Alberto <carlos98.camv@gmail.com>
2020-01-30 18:53:04 -0300

    Implemtaçao final

--- Exercicio_LendoDados_Linq/Program.cs
+++ Exercicio_LendoDados_Linq/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using Exercicio_LendoDados_Linq.Entidades;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace Exercicio_LendoDados_Linq
 {
@@ -6,7 +11,54 @@ namespace Exercicio_LendoDados_Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Funcionario> ListaFuncionarios = new List<Funcionario>();
+
+            Console.Write("Informe o caminho do arquivo que será lido: ");
+            string Caminho = Console.ReadLine();
+
+            Console.Write("Qual o valor do salarioa a ser lido: ");
+            double Valor = double.Parse(Console.ReadLine());
+
+            //Leitura e captaçao de dados
+            using (StreamReader sr = File.OpenText(Caminho))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] campos = sr.ReadLine().Split(',');
+                    string nome = campos[0];
+                    string email = campos[1];
+                    double salario = double.Parse(campos[2]);
+
+                    ListaFuncionarios.Add(new Funcionario(nome, email, salario));
+
+                }
+
+            }
+
+            var soma = ListaFuncionarios
+                .Where(p => p.Nome[0] == 'M')
+                .Sum(p => p.Salario);
+
+
+            var emails = ListaFuncionarios
+                .Where(p => p.Salario > Valor).OrderBy(p=>p.Nome)
+                .Select(p => p.Email);
+
+
+            Console.WriteLine("Email dos funcionarios com salarioa abaixo de: " + Valor.ToString("F2"));
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=");
+            foreach (string email in emails)
+            {
+                Console.WriteLine(email);
+            }
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.Write("Soma dos salarios dos funcionarios que começam com a letra 'M' " + soma.ToString("F2") );
+
+
+
+
+
         }
     }
 }
